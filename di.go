bcc54bc1c7aa0115655cd17f668f6aa45d2c2b38
Jas Hakimov <jasforever1@gmail.com>
2022-11-25 18:59:29 +0500

    first commit

---
+++ di.go
@@ -0,0 +1,76 @@
+package di
+
+import (
+	"errors"
+	"reflect"
+)
+
+type node struct {
+	obj interface{}
+	key string
+}
+
+var container []node
+
+func init() {
+	container = make([]node, 0)
+}
+
+func Register(obj interface{}) {
+	if !checkObjByKey(obj, "default") {
+		panic("Object have already exist")
+	}
+	container = append(container, node{
+		obj: obj,
+		key: "default",
+	})
+}
+
+func Get[T interface{}]() *T {
+	obj, err := getObjByKey[T]("default")
+	if err != nil {
+		panic(err.Error())
+	}
+	return obj
+}
+
+func RegisterWithName(obj interface{}, key string) {
+	if !checkObjByKey(obj, key) {
+		panic("Object have already exist")
+	}
+	container = append(container, node{
+		obj: obj,
+		key: key,
+	})
+}
+
+func GetByName[T interface{}](key string) *T {
+	obj, err := getObjByKey[T](key)
+	if err != nil {
+		panic(err.Error())
+	}
+	return obj
+}
+
+func checkObjByKey(obj interface{}, key string) bool {
+	for i := range container {
+		typesEqual := reflect.TypeOf(obj).String() == reflect.TypeOf(container[i].obj).String()
+		prEqual := key == container[i].key
+		if typesEqual && prEqual {
+			return false
+		}
+	}
+	return true
+}
+
+func getObjByKey[T interface{}](key string) (*T, error) {
+	for i := range container {
+		switch container[i].obj.(type) {
+		case *T:
+			if key == container[i].key {
+				return container[i].obj.(*T), nil
+			}
+		}
+	}
+	return nil, errors.New("object does not exist in container")
+}
